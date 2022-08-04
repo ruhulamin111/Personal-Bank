228fe86cbc0bc5025a07c4e164831741832dac9a
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-04 15:59:08 +0600

    withdraw and deposite

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.getElementById('submit').addEventListener('click', function (event) {
     const name = document.getElementById('name').value;
+    event.value = '';
     const password = document.getElementById('password').value;
-    if (name == 'bd@email.com' && password == '0000') {
+    if (name == 'bd@gmail.com' && password == '0000') {
         window.location.href = 'bank.html'
     }
 })
