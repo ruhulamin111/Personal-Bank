8e9fa7f4d0c85c25bca8ade4e095efa9ad617e5c
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-04 16:56:19 +0600

    sum of deposite and withdraw

--- bank.js
+++ bank.js
@@ -3,8 +3,23 @@ document.getElementById('deposite-submit').addEventListener('click', function (e
     const deposite = document.getElementById('deposite');
     depositeAmount = deposite.value;
     const depositeShow = document.getElementById('deposite-show');
-    depositeShow.innerText = depositeAmount;
+    const currentDeposite = depositeShow.innerText;
+    const updateDeposit = parseFloat(currentDeposite) + parseFloat(depositeAmount);
+    depositeShow.innerText = updateDeposit;
     deposite.value = '';
+
+
+})
+
+// withdraw system 
+document.getElementById('withdraw-submit').addEventListener('click', function (event) {
+    const withdraw = document.getElementById('withdraw');
+    withdrawAmount = withdraw.value;
+    const withdrawShow = document.getElementById('withdraw-show');
+    const currentWithdraw = withdrawShow.innerText;
+    const updateWithdraw = parseFloat(currentWithdraw) + parseFloat(withdrawAmount);
+    withdrawShow.innerText = updateWithdraw;
+    withdraw.value = '';
 
 
 })
