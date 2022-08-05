3db9277c30b1626e2be7b77e0afdf2079cfd3478
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-05 08:48:58 +0600

    calculation with function

--- bank.js
+++ bank.js
@@ -1,6 +1,29 @@
+
+// deposite with function 
+function getOutput(inputId) {
+    const deposite = document.getElementById(inputId);
+    depositeAmount = deposite.value;
+    const updateDeposit = parseFloat(depositeAmount);
+    deposite.value = '';
+    return updateDeposit;
+}
+
+
 // deposite system 
 document.getElementById('deposite-submit').addEventListener('click', function (event) {
-    const deposite = document.getElementById('deposite');
+    const depositeAmount = getOutput('deposite')
+    const depositeShow = document.getElementById('deposite-show');
+    const currentDeposite = depositeShow.innerText;
+    const updateDeposit = parseFloat(currentDeposite) + depositeAmount;
+    depositeShow.innerText = updateDeposit;
+
+    const balanceShow = document.getElementById('balance-show');
+    const balanceAmount = balanceShow.innerText;
+    const updateBalance = parseFloat(balanceAmount) + parseFloat(depositeAmount);
+    balanceShow.innerText = updateBalance;
+    deposite.value = '';
+
+    /* const deposite = document.getElementById('deposite');
     depositeAmount = deposite.value;
     const depositeShow = document.getElementById('deposite-show');
     const currentDeposite = depositeShow.innerText;
@@ -11,13 +34,26 @@ document.getElementById('deposite-submit').addEventListener('click', function (e
     const balanceAmount = balanceShow.innerText;
     const updateBalance = parseFloat(balanceAmount) + parseFloat(depositeAmount);
     balanceShow.innerText = updateBalance;
-    deposite.value = '';
+    deposite.value = ''; */
 
 })
 
 // withdraw system 
 document.getElementById('withdraw-submit').addEventListener('click', function (event) {
-    const withdraw = document.getElementById('withdraw');
+    const withdrawAmount = getOutput('withdraw');
+    const withdrawShow = document.getElementById('withdraw-show');
+    const currentWithdraw = withdrawShow.innerText;
+    const updateWithdraw = parseFloat(currentWithdraw) + withdrawAmount;
+    withdrawShow.innerText = updateWithdraw;
+
+    const balanceShow = document.getElementById('balance-show');
+    const balanceAmount = balanceShow.innerText;
+    const updateBalance = parseFloat(balanceAmount) - parseFloat(withdrawAmount);
+    balanceShow.innerText = updateBalance;
+    withdraw.value = '';
+
+
+    /* const withdraw = document.getElementById('withdraw');
     withdrawAmount = withdraw.value;
     const withdrawShow = document.getElementById('withdraw-show');
     const currentWithdraw = withdrawShow.innerText;
@@ -28,6 +64,7 @@ document.getElementById('withdraw-submit').addEventListener('click', function (e
     const balanceAmount = balanceShow.innerText;
     const updateBalance = parseFloat(balanceAmount) - parseFloat(withdrawAmount);
     balanceShow.innerText = updateBalance;
-    withdraw.value = '';
+    withdraw.value = ''; */
 
 })
+
